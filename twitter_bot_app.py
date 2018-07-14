5b71a56b8c0fa477e0f1122a0b6cc3db894f7ea5
Naveen Shandilya <40190745+naveenshandilya30@users.noreply.github.com>
2018-07-14 15:37:41 +0545

    Final Project

--- twitter_bot_app.py
+++ twitter_bot_app.py
@@ -14,7 +14,6 @@ import textblob
 from textblob import TextBlob
 import  flag
 
-
 auth= tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_secret)
 api=tweepy.API(auth)
@@ -22,16 +21,17 @@ api=tweepy.API(auth)
 paralleldots.set_api_key("BJnIQFVlvmYwoWLVYltgzsV01JKT5nRsSgg36OEVey0")
 paralleldots.get_api_key()
 
-
+#to retrive tweets
 def retrieve_tweets():
     global tweets
-    tweet_input = input("enter the hashtag you want to search")
+    tweet_input = input("Enter the hashtag you want to search :")
     tweets = api.search(q=tweet_input)
     print(tweets)
 
 
+#count no. of followers
 def f_count():
-    tweet_input = input("Enter the hashtag you want to search")
+    tweet_input = input("Enter the hashtag you want to search :")
     tweets = api.search(q=tweet_input)
     print("UserName      Followers Count")
 
@@ -39,20 +39,15 @@ def f_count():
         print(tweet.user.name + "     " + str(tweet.user.followers_count))
 
 
-
+#To update the status
 def update_status():
-    update_status=api.update_status(status ="Hi people...!")
+    update_status=api.update_status(status ="Hi friends...!")
     print(update_status)
 
-def tweet_location():
-    tweets=input("Enter the hashtag  you want to search")
-    tweet1=api.search(tweets)
-    for search_results in tweet1:
-        print('location',search_results.user.location)
-        print('time_zone',search_results.user.time_zone)
 
+#Sentiments
 def sentiments():
-    tweet_input = input("Enter the hashtag you want to search")
+    tweet_input = input("Enter the hashtag you want to search: ")
     tweets = api.search(q=tweet_input)
     print(tweets)
 
@@ -60,7 +55,17 @@ def sentiments():
         text = tweet.text
 
     print("\nSentiments")
-    print(paralleldots.sentiment("i'am very happy")["sentiment"])
+    print(paralleldots.sentiment("Hi ")["sentiment"])
+
+#location ,timezone,lang of tweet
+
+def tweet_location():
+    tweets=input("Enter the hashtag  you want to search")
+    tweet1=api.search(tweets)
+    for search_results in tweet1:
+        print('location',search_results.user.location)
+        print('time_zone',search_results.user.time_zone)
+        print('language', search_results.user.lang)
 
 
 def stwords():
@@ -73,7 +78,6 @@ def stwords():
         tmp = []
         tmp.append(fulltext)
         temp = tmp
-        import re
         cur_tweet = re.sub(r"http\S+", "", str(temp))
         cur_tweet1 = re.split(r"\s", cur_tweet)
         cur_tweet = [w for w in cur_tweet1 if not w in stop_words]
@@ -84,17 +88,62 @@ def stwords():
                 count = Counter(cur_tweet).most_common(10)
         print(count)
 
-#MENU
+#Compare tweets
+
+def compare():
+    country1 = 0
+    country2 = 0
+
+    #Country 1
+
+    tweets = api.user_timeline(screen_name="narendramodi", count=200, tweet_mode="extended")
+    for tweet_compare in tweets:
+        fulltext = tweet_compare.full_text
+        tmp = []
+        tmp.append(fulltext)
+        temp = tmp
+        import re
+        cur_tweet = re.sub(r"http\S+", "", str(temp))
+        cur_tweet = re.split(r"\s", cur_tweet)
+        for word in cur_tweet:
+            word=word.upper()
+            if word == "AMERICA" or word == "US" or word=="USA":
+                country1 = country1 + 1
+    print("America BY NARENDRA MODI: "+ str(country1))
+
+    #compare with Country2
+
+    tweets = api.user_timeline(screen_name="realDonaldTrump", count=200, tweet_mode="extended")
+    for tweet_compare in tweets:
+        fulltext = tweet_compare.full_text
+        tmp = []
+        tmp.append(fulltext)
+        temp = tmp
+        import re
+        cur_tweet = re.sub(r"http\S+", "", str(temp))
+        cur_tweet = re.split(r"\s", cur_tweet)
+        for word in cur_tweet:
+            word = word.upper()
+            if word == "INDIA":
+                country2 = country2 + 1
+    print("INDIA BY DONALD TRUMP: " + str(country2))
+
+
+
+
+#DISPLAY MENU
 def display_menu():
-    print('''Select an option you want to perform :
+    print('''select the option you want to perform :
     1.)To Retrieve  the Tweets..
     2.)To Count the number of Followers..
     3.)To Update the Status..
-    4.)To find the Sentiments..
+    4.)To Find the Sentiments..
     5.)To Find location and time zone of tweet..
-    6.) EXIT the Menu.......
+    6.)To Determine top usage..
+    7.)To Compare the tweets...
+    8.) EXIT the Menu.......
     ''')
-    option=int(input("Please enter the option you want to retrieve from the given  MENU: "))
+    option=int(input("Please enter the option you want to retrieve from MENU: "))
     if (option==1):
         retrieve_tweets()
         display_menu()
@@ -115,8 +164,16 @@ def display_menu():
         tweet_location()
         display_menu()
 
-    elif option == 6:
-        print("Good Byeeeeeee....🖐🖐")
+    elif (option == 6):
+        stwords()
+        display_menu()
+
+    elif (option == 7):
+        compare()
+        display_menu()
+
+    elif option == 8:
+        print("Good Byeee friend....🖐🖐")
         flag = False
     else:
         print("Please enter the valid input ")
